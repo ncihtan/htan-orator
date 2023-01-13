303f1866ac5ecf86e405e9ae21635d333eb54e07
Adam Taylor <adam.taylor@sagebase.org>
2023-01-13 13:13:54 +0000

    Make imaging specific output conditional on component

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ orator.orate('syn24829433')
 
 returns
 
-> 'HTA9_1_19362 is a mIHC image of a biopsy (Biospecimen HTA9_1_17) from a 70 female (Participant HTA9_1) diagnosed with infiltrating duct carcinoma NOS. The image contains 12 channels, approximately 8.96M pixels, and measures 1939 µm wide by 1157 µm high'
+> 'HTA9_1_19362 is a mIHC image submitted by the HTAN OHSU center of a biopsy (Biospecimen HTA9_1_17) from a 70 female (Participant HTA9_1) diagnosed with infiltrating duct carcinoma NOS. 
+> The image contains 12 channels, approximately 8.96M pixels, and measures 1939 µm wide by 1157 µm high. It was acquired on a Leica, Aperio AT2 at 20x magnification
 
 ### Conda environment
 
--- orator.py
+++ orator.py
@@ -7,10 +7,10 @@ bigquery_client = bigquery.Client()
 
 import synapseclient
 syn = synapseclient.Synapse()
-syn.login()
+syn.login(silent=True)
 
 def get_bq_table ( id, id_col, component):
-    print(f'Getting {component} information from {id}')
+    #print(f'Getting {component} information from {id}')
 
     query = f"""
     SELECT * FROM `htan-dcc.combined_assays.{component}`
@@ -26,8 +26,6 @@ def days_to_age (days):
     return(age)
 
 
-
-
 def human_format(number):
     units = ['', 'K', 'M', 'G', 'T', 'P']
     k = 1000.0
@@ -39,6 +37,24 @@ def first_lower(s):
         return 
     else:
         return s[0].lower() + s[1:]
+
+htan_centers = {
+    "HTA1": "HTAN HTAPP",
+    "HTA2": "PCAPP Pilot Project",
+    "HTA3": "HTAN BU",
+    "HTA4": "HTAN CHOP",
+    "HTA5": "HTAN DFCI",
+    "HTA6": "HTAN Duke",
+    "HTA7": "HTAN HMS",
+    "HTA8": "HTAN MSK",
+    "HTA9": "HTAN OHSU",
+    "HTA10": "HTAN Stanford",
+    "HTA11": "HTAN Vanderbilt",
+    "HTA12": "HTAN WUSTL",
+    "HTA13": "HTAN TNP SARDANA",
+    "HTA14": "HTAN TNP - TMA",
+    "HTA14": "HTAN SRRS"
+}
 
 def orate(synid: str):
     '''Takes a Synapse ID of a HTAN Data File and returns a natural language description of the dataset'''
@@ -61,15 +77,29 @@ def orate(synid: str):
     # Get diagnosis information
     diagnosis = get_bq_table(participant_id, 'HTAN_Participant_Id', 'Diagnosis')
 
-    oration =  (
-        f"{annotations['HTANDataFileID'][0]} is a {annotations['ImagingAssayType'][0]} image of a {first_lower(biospecimen['Acquisition_Method_Type'][0])} "
-        f"(Biospecimen {biospecimen_id}) "
+    # Look up the HTAN Center
+
+    center_id = re.match('HTA\d+', biospecimen_id).group(0)
+
+    general =  (
+        f"{annotations['HTANDataFileID'][0]} is a {annotations['ImagingAssayType'][0]} image submitted by the {htan_centers[center_id]} center "
+        f"of a {first_lower(biospecimen['Acquisition_Method_Type'][0])} (Biospecimen {biospecimen_id}) "
         f"from a {days_to_age(biospecimen['Collection_Days_from_Index'][0])} {demographics['Gender'][0]} "
         f"(Participant {participant_id}) "
         f"diagnosed with {first_lower(diagnosis['Primary_Diagnosis'][0])}. "
-        f"The image contains {annotations['SizeC'][0]} channels, approximately {human_format(pixels)} pixels, and measures "
-        f"{ceil(int(annotations['SizeX'][0])*float(annotations['PhysicalSizeX'][0]))} {annotations['PhysicalSizeXUnit'][0]} wide "
-        f"by {ceil(int(annotations['SizeY'][0])*float(annotations['PhysicalSizeY'][0]))} {annotations['PhysicalSizeYUnit'][0]} high"
-
     )
+
+    if annotations['Component'][0] == 'ImagingLevel2':
+        imaging = (
+            f"The image contains {annotations['SizeC'][0]} channels, approximately {human_format(pixels)} pixels, and measures "
+            f"{ceil(int(annotations['SizeX'][0])*float(annotations['PhysicalSizeX'][0]))} {annotations['PhysicalSizeXUnit'][0]} wide "
+            f"by {ceil(int(annotations['SizeY'][0])*float(annotations['PhysicalSizeY'][0]))} {annotations['PhysicalSizeYUnit'][0]} high. "
+            f"It was acquired on a {annotations['Microscope'][0]} microscope at {annotations['NominalMagnification'][0]}x magnification"
+        )
+
+        oration = "\n".join([general, imaging])
+    
+    else:
+        oration = general
+
     return(oration)
